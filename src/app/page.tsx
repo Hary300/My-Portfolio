9cee90d58f8a3195416ddf8d4e821db1e759a5da
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-07-15 03:48:31 +0700

    feat: update dependencies and add new components for layout and sections
    - Added new dependencies: class-variance-authority, clsx, lucide-react, motion, radix-ui, react-icons, shadcn, tailwind-merge, and tw-animate-css.
    - Refactored globals.css to include new styles and custom properties.
    - Updated layout.tsx to use Red Hat Display font and improved class management.
    - Enhanced page.tsx to include multiple sections: Navbar, Hero, Services, About Me, Skills, Working, Experience, Projects, Testimonials, FAQ, and Contact.
    - Created new components for layout (Container, Footer, Navbar) and sections (AboutMe, Contact, Experience, Faq, Hero, Projects, Service, Skills, Testimonials, Working).
    - Implemented a mobile menu using Radix UI's Sheet component.
    - Added utility functions for class name management.
    - Introduced navigation links data structure for easy management of links in the Navbar.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+import Footer from '@/components/layouts/Footer';
+import Navbar from '@/components/layouts/Navbar';
+import AboutMeSection from '@/components/sections/AboutMeSection';
+import ContactSection from '@/components/sections/ContactSection';
+import ExperienceSection from '@/components/sections/ExperienceSection';
+import FaqSection from '@/components/sections/FaqSection';
+import HeroSection from '@/components/sections/HeroSection';
+import ProjectsSection from '@/components/sections/ProjectsSection';
+import ServiceSection from '@/components/sections/ServiceSection';
+import SkillsSection from '@/components/sections/SkillsSection';
+import TestimonialsSection from '@/components/sections/TestimonialsSection';
+import WorkingSection from '@/components/sections/WorkingSection';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hary300</h1>
-    </div>
+    <>
+      <Navbar />
+      <main>
+        <HeroSection />
+        <ServiceSection />
+        <AboutMeSection />
+        <SkillsSection />
+        <WorkingSection />
+        <ExperienceSection />
+        <ProjectsSection />
+        <TestimonialsSection />
+        <FaqSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </>
   );
 }
